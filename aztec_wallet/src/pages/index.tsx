8045c8bd220857120854463af0c9576f6da45630
ronykris <rony.kris@gmail.com>
2024-08-27 22:35:23 +0530

    added wallet ui

--- aztec_wallet/src/pages/index.tsx
+++ aztec_wallet/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { setupSandbox } from "../utils/aztec/pxe_connect";
 import { useEffect, useState } from "react";
+import { WalletUI } from "../components/WalletUi"
 
+type CompleteAddress = any;
 
 export default function Home() {
-  const [registeredAccounts, setRegisteredAccounts] = useState(null);
-  const [blockNum, setBlockNum] = useState(null);
-  const [error, setError] = useState(null);
+  const [registeredAccounts, setRegisteredAccounts] = useState<CompleteAddress[] | null>(null);
+  const [blockNum, setBlockNum] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() =>{
     const fetchData = async () => {
@@ -17,8 +19,12 @@ export default function Home() {
         setRegisteredAccounts(registeredAccounts)
         setBlockNum(blockNum)
       } catch (e) {
-        setError(e.message);
-        console.error("Error setting up sandbox:", e);
+        if (e instanceof Error) {
+          setError(e.message);
+          console.error("Error setting up sandbox:", e);
+        } else {
+          console.error("Unknown error:", e);
+        }
       }
     }
     fetchData()
@@ -27,15 +33,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl">Home Page</h1>
+      <WalletUI />
+      {/*
       {error && <p>Error: {error}</p>}
       {!error && registeredAccounts && blockNum && (
         <>
           <p>Registered Accounts: {JSON.stringify(registeredAccounts)}</p>
-          <p>Block Number: {blockNum}</p>
+          <p>Block Number: {blockNum}</p>          
         </>
       )}
       {!error && (!registeredAccounts || !blockNum) && <p>Loading...</p>}
             {/* <WalletUI/> */}
+      
     </main>
   );
 }
